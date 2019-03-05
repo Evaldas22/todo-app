cce06e03493e030fc9031aae380190b97dfe4bd2
Evaldas <surauciusevaldas@gmail.com>
2019-03-05 21:54:17 +0200

    Remove initial state with mock data

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,7 +1,6 @@
 import { combineReducers } from "redux";
 import todoList from './todoList';
 
-
 const rootReducer = combineReducers({
     todoList
 });
--- client/src/store/index.js
+++ client/src/store/index.js
@@ -5,26 +5,6 @@ import logger from 'redux-logger';
 // Redux dev tools stuff
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
-const intialState = {
-    todoList: [
-        {
-            todoId: 1,
-            text: 'Wash dishes',
-            checked: false
-        },
-        {
-            todoId: 2,
-            text: 'Shop for groceries',
-            checked: false
-        },
-        {
-            todoId: 3,
-            text: 'Wash clothes',
-            checked: false
-        }
-    ]
-}
-
-const store = createStore(rootReducer, intialState, composeEnhancers(applyMiddleware(logger)));
+const store = createStore(rootReducer, composeEnhancers(applyMiddleware(logger)));
 
 export default store;
