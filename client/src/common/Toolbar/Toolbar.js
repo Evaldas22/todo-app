5ddd51ebdccacca2808b4789490332d03382d942
Evaldas <surauciusevaldas@gmail.com>
2019-03-10 18:32:39 +0200

    Add styling to toolbar

--- client/src/common/Toolbar/Toolbar.js
+++ client/src/common/Toolbar/Toolbar.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import './Toolbar.css';
 
 const Toolbar = props => (
-    <header>
-        <nav>
-            <div><a href="#">TODO manager</a></div>
-            <div>
+    <header className="toolbar">
+        <nav className="toolbar-nav">
+            <div className="toolbar-name"><a href="#">TODO manager</a></div>
+            <div className="spacer"></div>
+            <div className="toolbar-nav-items">
                 <ul>
                     <li><a href="#">My TODOs</a></li>
-                    <li><a href="#">Sign in</a></li>
-                    <li><a href="#">Sign up</a></li>
+                    <li><a href="#">{props.username ? `Welcome ${props.username}` : "Sign in"}</a></li>
+                    {props.username ? null : (<li><a href="#">Sign up</a></li>)}
                 </ul>
             </div>
         </nav>
