18bbcfd8a81396bc772b462c1ba1be322578c79c
Evaldas <surauciusevaldas@gmail.com>
2019-03-10 18:47:29 +0200

    Add more routes

--- client/src/common/Toolbar/Toolbar.js
+++ client/src/common/Toolbar/Toolbar.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import './Toolbar.css';
+import { Link } from 'react-router-dom';
 
 const Toolbar = props => (
     <header className="toolbar">
         <nav className="toolbar-nav">
-            <div className="toolbar-name"><a href="#">TODO manager</a></div>
+            <div className="toolbar-name"><Link to="/" >TODO Manager</Link></div>
             <div className="spacer"></div>
             <div className="toolbar-nav-items">
                 <ul>
-                    <li><a href="#">My TODOs</a></li>
-                    <li><a href="#">{props.username ? `Welcome ${props.username}` : "Sign in"}</a></li>
-                    {props.username ? null : (<li><a href="#">Sign up</a></li>)}
+                    <li><Link to="/" >My TODOs</Link></li>
+                    <li>{props.username ? 
+                        `Welcome, ${props.username}!` : 
+                        (<Link to="/signIn">Sign in</Link>)}</li>
+                    {props.username ? null : (<li><Link to="/signUp">Sign up</Link></li>)}
                 </ul>
             </div>
         </nav>
--- client/src/index.js
+++ client/src/index.js
@@ -6,12 +6,18 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './store';
-import { BrowserRouter, Route } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { SignIn } from './components/SignIn/SignIn';
+import { SignUp } from './components/SignUp/SignUp';
 
 ReactDOM.render((
     <Provider store={store}>
         <BrowserRouter>
-            <Route path="/" component={App} />
+            <Switch>
+                <Route exact path="/" component={App} />
+                <Route exact path="/signIn" component={SignIn} />
+                <Route exact path="/signUp" component={SignUp} />
+            </Switch>
         </BrowserRouter>
     </Provider>
     ),
